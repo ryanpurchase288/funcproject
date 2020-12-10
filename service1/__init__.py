43008b39d41fd93256d83a816fd48dbde3044e37
Ubuntu <rpurchase@vm1.rk1covdy23nuhljgav30d430me.zx.internal.cloudapp.net>
2020-12-10 15:48:03 +0000

    completed the task

--- service1/__init__.py
+++ service1/__init__.py
@@ -6,15 +6,15 @@ from azure.cosmos import CosmosClient, PartitionKey
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    letterstring = requests.get('https://appserveless.azurewebsites.net/api/service2?code=1sfWuUECuyjhfR5LyHuNHNTA3aL7a2MgG9EnBMVMnXdDvALzUTWWMA==').text
-    nostring = requests.get('https://appserveless.azurewebsites.net/api/service3?code=SNmyvhsMyHksv1sj60Drqqa2i819tm2aulUBL4YJCpemkyB0ZQOeSA==').text
+    letterstring = requests.get('https://appserverless.azurewebsites.net/api/service2?code=aIjyCfy/YyF127KlciSzC7NrwNfqn1waGSZbZ0kQZnmlYjznQIUO5g==').text
+    nostring = requests.get('https://appserverless.azurewebsites.net/api/service3?code=OYvFXuUAzEaZ85d5nJSwaEWq1Dcv6XCvR6VhNmdXwyArxVLdUT4MwA==').text
     username = ""
     for i in range(5):
         username += letterstring[i]
         username += nostring[i]
 
     endpoint = "https://ryan.documents.azure.com:443/"
-    key = "vHfiVCGlnLpYIK03FcB6uiJAHLnjoOhQFCjHRHwr2sdJ8yeTRcRNT5tty3zMgZCQwY0rgeIITF0sx2TRycbkAA=="
+    key = "tmWgihjw68kOrkMSzMCdU8JOaj1ofw9mmxujRnBLedHAMMHFwery3brqMRlrV2HDOj6p1sBLWx5XjwB1GUAiVA=="
     client = CosmosClient(endpoint, key)
 
     database_name = "Usernames"
@@ -23,7 +23,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     container_name = "UsernameContainer"
     container = database.create_container_if_not_exists(
         id=container_name,
-        partition_key=PartitionKey(path="/username")
+        partition_key=PartitionKey(path="/username"),
         offer_throughput=400
     )
     username_to_add = {
